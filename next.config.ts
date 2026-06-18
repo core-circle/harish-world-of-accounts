7baa330d7ffdeef57017eb1625346b461a0dd7e6
srinivasan <1694srinivasan@gmail.com>
2026-06-18 23:51:27 +0530

    fix asset paths for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/harish-world-of-accounts" : undefined,
   distDir: process.env.NODE_ENV === "production" ? "dist" : undefined,
   images: {
     unoptimized: true,
